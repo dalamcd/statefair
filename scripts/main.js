d4a06194ca216a53ba6a93651a41c23302c213c5
dalamcd <dalamcd@gmail.com>
2020-10-21 10:24:27 -0500

    Added visual representation of rider

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,12 @@
 // Import and invoke the ticket booth component function
 
+import { RideTicketHolders } from "./rides/RideTicketHolders.js";
 import { TicketBooth } from "./TicketBooth.js";
 
 const eventHub = document.querySelector("#state-fair")
 
 TicketBooth();
-
-eventHub.addEventListener("ridePicked", e => {
-    console.log("w00t");
-})
+RideTicketHolders();
+// eventHub.addEventListener("ridePicked", e => {
+//     console.log("w00t");
+// })
