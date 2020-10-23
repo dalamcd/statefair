4ed70d2d5e2b2bb0ad92dbe75d5e7c59f7c52d69
dalamcd <dalamcd@gmail.com>
2020-10-22 23:58:37 -0500

    Fixed bugs with vistor count

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -2,11 +2,6 @@ const contentTarget = document.querySelector(".entry")
 const eventHub = document.querySelector("#state-fair")
 let tickets = 0;
 
-eventHub.addEventListener("buttonClicked", e => {
-    tickets++
-    TicketBoothVisitors();
-})
-
 export const TicketBooth = () => {
     contentTarget.innerHTML = `
         <div class="ticketBooth">
@@ -32,20 +27,27 @@ const TicketBoothVisitors = () => {
     visitorTarget.innerHTML = `Number of visitors: ${tickets}`;
 }
 
-eventHub.addEventListener("click", e => {
-    const customEvent = new CustomEvent( "buttonClicked", { detail: {} } );
+eventHub.addEventListener("click", e => {    
+    const customEvent = new CustomEvent("buttonClicked", { detail: {} });
 
-    if(e.target.id === "rideTicket")
+    if (e.target.id === "rideTicket")
         customEvent.detail.button = "ride"
-    if(e.target.id === "foodTicket")
+    if (e.target.id === "foodTicket")
         customEvent.detail.button = "food"
-    if(e.target.id === "gameTicket")
+    if (e.target.id === "gameTicket")
         customEvent.detail.button = "game"
-    if(e.target.id === "sideshowTicket")
+    if (e.target.id === "sideshowTicket")
         customEvent.detail.button = "sideshow"
-    if(e.target.id === "bigSpenderTicket") {
+    if (e.target.id === "bigSpenderTicket")
         customEvent.detail.button = "bigSpender"
-        console.log("bigspender event")
-    }
-        eventHub.dispatchEvent(customEvent);
+
+    eventHub.dispatchEvent(customEvent);
 })
+
+eventHub.addEventListener("buttonClicked", e => {
+
+    if (e.detail.button) {
+        tickets++;
+        TicketBoothVisitors();
+    }
+});
